0344244e546c893b44409bdb94f4ee32f010c357
Nikhil <nick11ks@gmail.com>
2023-01-12 19:36:18 -0500

    Initial commit

---
+++ src/main/java/com/teachofax/teachershubabi/TeachersHubAbiApplication.java
@@ -0,0 +1,13 @@
+package com.teachofax.teachershubabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachersHubAbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachersHubAbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teachofax/teachershubabi/TeachersHubAbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachofax.teachershubabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachersHubAbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
